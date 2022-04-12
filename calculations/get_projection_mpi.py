8741a8f6a5ac3805114d5cd709dd8bb785a57e69
HinnyTsang <mhtsangab@connect.ust.hk>
2022-04-12 18:08:45 +0800

    modified output structure

--- calculations/get_projection_mpi.py
+++ calculations/get_projection_mpi.py
@@ -1,23 +1,15 @@
 from typing import Tuple
-from xmlrpc.client import Boolean
 from mpi4py import MPI
 import h5py
 import numpy as np
+import os
 
 import sys
 sys.path.append('..')
-import functions.calc_mcf as calc_mcf
-import functions.calc_projection as cp
-import functions.calc_unit_conversion as uc
 import functions.calc_orientation as calc_ori
-
-from operator import pos
-from os.path import exists
-
-
-
-
-
+import functions.calc_unit_conversion as uc
+import functions.calc_projection as cp
+import functions.calc_mcf as calc_mcf
 
 
 # initialize MPI
@@ -25,8 +17,8 @@ comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-
-print(f"Number of threads is {size}.")
+if rank == 0:
+    print(f"Number of threads is {size}.")
 
 
 def read_data(file):
@@ -50,22 +42,40 @@ def main(file: str, n_rand: int, method: str) -> None:
     # set random seed
     np.random.seed(0)
 
-    if not exists(file):
-        print("file {file} not exist.")
+    if not os.path.exists(file):
+        if rank == 0:
+            print(f"file {file} not exist.")
         return
-    
+
     if method == "fib":
-        print("generating uniform projection")
+        if rank == 0:
+            print("generating uniform projection")
         x_rand_, y_rand_, z_rand_ = cp.fibonacci_sphere(n_rand)
 
     elif method == "ran":
-        print("generating random projection")
+        if rank == 0:
+            print("generating random projection")
         x_rand_, y_rand_, z_rand_ = cp.random_unit_vector(n_rand)
 
     else:
-        print("arg should be either 'ran' or 'fib'.")
+        if rank == 0:
+            print("arg should be either 'ran' or 'fib'.")
         return
 
+    # TODO Check if output path exist, create if not exist. #####
+    path, file_name = os.path.split(file)
+    out_path = os.path.splitext(file_name)[0] + f"_{n_rand}_{method}" # output path.
+    out_path = os.path.join(path, out_path).replace('/h5_max_contour/', '/h5_projected/')
+    
+    if os.path.exists(out_path):
+        if rank == 0: print(f"output path {out_path} exist")
+    else:
+        if rank == 0: 
+            print(f"output path {out_path} is not exist, create output path.")
+            os.mkdir(out_path)
+    #############################################################
+    
+    
     # number of projection per processor
     nRandPerProc = int(n_rand/size)
 
@@ -79,36 +89,29 @@ def main(file: str, n_rand: int, method: str) -> None:
     y_rand = y_rand_[init:term]
     z_rand = z_rand_[init:term]
 
-    # Read data
 
     # MPI read data.
     data = None
 
     if rank == 0:
-        print(rank, f"Reading data from {file}")
+        print(f"Reading data from {file}")
         data = read_data(file)
-        print(rank, "Finish reading data")
-        for key in data.keys():
-            print(key, type(data[key]))
+        print("Finish reading data")
 
     # boardcast data.
     comm.Barrier()
     data = comm.bcast(data, root=0)
     comm.Barrier()
 
-    #         np.array(all_cloud_orientation), np.array(all_b_field_orientation), \
-    #         np.array(all_mcf), np.array(all_mcf_bin), np.array(all_mcf_slope), np.array(all_mcf_area), \
-    #         np.array(all_aspect_ratio), np.array(all_cloud_masses), \
-    #         np.array(all_min_den), np.array(all_max_den), \
-    #         np.array(binned_den), np.array(all_stoke_Q_map), np.array(all_stoke_U_map)
+    # TODO Call the projection function ########################################
     cloud_orientation, b_orientation, \
-        mcf, mcf_bin, mcf_slope, mcf_area, \
+        mcf_slope, mcf_area, \
         aspect_ratio, cloud_mass, min_den, max_den, \
-        binned_den, binned_stoke_Q, binned_stoke_U \
-        = calc_all_projections(data, x_rand, y_rand, z_rand)
+        = calc_all_projections(data, x_rand, y_rand, z_rand, out_path)
+    ############################################################################
+    # the last arguement file and method is not necessary.
 
-
-    # LOS 
+    # LOS
     xRandRecvbuf = comm.gather(x_rand, root=0)
     yRandRecvbuf = comm.gather(y_rand, root=0)
     zRandRecvbuf = comm.gather(z_rand, root=0)
@@ -116,8 +119,6 @@ def main(file: str, n_rand: int, method: str) -> None:
     cloudThetaRecvbuf = comm.gather(cloud_orientation, root=0)
     posBThetaRecvbuf = comm.gather(b_orientation, root=0)
     # MCF
-    MCFRecvbuf = comm.gather(mcf, root=0)
-    MCFBinsRecvbuf = comm.gather(mcf_bin, root=0)
     MCFSlopeRecvbuf = comm.gather(mcf_slope, root=0)
     MCFAreaRecvbuf = comm.gather(mcf_area, root=0)
     # Cloud properties
@@ -125,12 +126,8 @@ def main(file: str, n_rand: int, method: str) -> None:
     cloudMassRecvbuf = comm.gather(cloud_mass, root=0)
     minDenRecvbuf = comm.gather(min_den, root=0)
     maxDenRecvbuf = comm.gather(max_den, root=0)
-    # Projection map
-    DensityRecvbuf = comm.gather(binned_den, root=0)
-    StokeQRecvbuf = comm.gather(binned_stoke_Q, root=0)
-    StokeURecvbuf = comm.gather(binned_stoke_Q, root=0)
-    
-    
+
+
     # Create output file.
     if rank == 0:
         # LOS
@@ -140,9 +137,7 @@ def main(file: str, n_rand: int, method: str) -> None:
         # Orientation
         cloudThetaRecvbuf = np.reshape(cloudThetaRecvbuf, n_rand)
         posBThetaRecvbuf = np.reshape(posBThetaRecvbuf, n_rand)
-        # MCF 
-        MCFRecvbuf = np.reshape(MCFRecvbuf, n_rand)
-        MCFBinsRecvbuf = np.reshape(MCFBinsRecvbuf, n_rand)
+        # MCF
         MCFSlopeRecvbuf = np.reshape(MCFSlopeRecvbuf, n_rand)
         MCFAreaRecvbuf = np.reshape(MCFAreaRecvbuf, n_rand)
         # Cloud properties
@@ -151,14 +146,11 @@ def main(file: str, n_rand: int, method: str) -> None:
         minDenRecvbuf = np.reshape(minDenRecvbuf, n_rand)
         maxDenRecvbuf = np.reshape(maxDenRecvbuf, n_rand)
         # Projection map
-        DensityRecvbuf = np.reshape(DensityRecvbuf, n_rand)
-        StokeQRecvbuf = np.reshape(StokeQRecvbuf, n_rand)
-        StokeURecvbuf = np.reshape(StokeURecvbuf, n_rand)
 
         print("Writing output file ...")
 
         # First file storing 1d projected data
-        with h5py.File(file.replace(".h5", f"_{n_rand:d}_{method}_main.h5").replace('/h5/', '/h5_projected/'), 'w-') as write_data:
+        with h5py.File(os.path.join(out_path, 'main.h5'), 'w-') as write_data:
 
             # Parameters TODO
             # LOS
@@ -166,7 +158,8 @@ def main(file: str, n_rand: int, method: str) -> None:
             write_data.create_dataset('los_y', data=yRandRecvbuf)
             write_data.create_dataset('los_z', data=zRandRecvbuf)
             # Orientation
-            write_data.create_dataset('cloud_orientation', data=cloudThetaRecvbuf)
+            write_data.create_dataset(
+                'cloud_orientation', data=cloudThetaRecvbuf)
             write_data.create_dataset('b_orientation', data=posBThetaRecvbuf)
             # MCF
             write_data.create_dataset('mcf_slope', data=MCFSlopeRecvbuf)
@@ -178,23 +171,9 @@ def main(file: str, n_rand: int, method: str) -> None:
             write_data.create_dataset('max_den', data=maxDenRecvbuf)
             write_data.close()
 
-        # Second file, storing the projected map.
-        with h5py.File(file.replace(".h5", f"_{n_rand:d}_{method}_map.h5").replace('/h5/', '/h5_projected/'), 'w-') as write_data:
-            # projection maps
-            write_data.create_dataset('den', DensityRecvbuf)
-            write_data.create_dataset('stoke_Q', StokeQRecvbuf)
-            write_data.create_dataset('stoke_U', StokeURecvbuf)
-            write_data.close()
-            
-        # third file, storing the mcf
-        with h5py.File(file.replace(".h5", f"_{n_rand:d}_{method}_mcf.h5").replace('/h5/', '/h5_projected/'), 'w-') as write_data:
-            # mcfs.
-            write_data.create_dataset('mcf', MCFRecvbuf)
-            write_data.create_dataset('mcf_bin', MCFBinsRecvbuf)
-            write_data.close()
-    
 def calc_all_projections(
-    data: dict, losX: np.ndarray, losY: np.ndarray, losZ: np.ndarray
+    # for file output, not necessary
+    data: dict, losX: np.ndarray, losY: np.ndarray, losZ: np.ndarray, out_path: str
 ) -> Tuple:
     """
         calc projections for a lot of line of sight.
@@ -206,19 +185,14 @@ def calc_all_projections(
 
         :return:  tubple of the following parameters
                     (
-                        np.array(all_cloud_orientation), 
+                        np.array(all_cloud_orientation),
                         np.array(all_b_field_orientation),
-                        np.array(all_mcf), 
-                        np.array(all_mcf_bin), 
-                        np.array(all_mcf_slope), 
+                        np.array(all_mcf_slope),
                         np.array(all_mcf_area),
-                        np.array(all_aspect_ratio), 
+                        np.array(all_aspect_ratio),
                         np.array(all_cloud_masses),
-                        np.array(all_min_den), 
-                        np.array(all_max_den),
-                        np.array(binned_den), 
-                        np.array(all_stoke_Q_map), 
-                        np.array(all_stoke_U_map)
+                        np.array(all_min_den),
+                        np.array(all_max_den)
                     )
     """
 
@@ -244,17 +218,16 @@ def calc_all_projections(
     all_stoke_U_map = []
     ###########################################
 
-    cnt = 0
+    cnt = 1
     # Calculate all step for each los
     for los in zip(losX, losY, losZ):
 
         print(f"rank {rank:2d}, cnt = {cnt:4d}")
-        cnt += 1
-        theta, phi = uc.cartToSph(*los)
+        theta, phi = cp.cart_to_sph(*los)
         sys.stdout.flush()
 
         # TODO 1 Rotate data py lOS ###########################################
-        x, y, z = cp.rotate_3d(data['bx'], data['by'], data['bz'], *los)
+        x, y, z = cp.rotate_3d(data['x'], data['y'], data['z'], *los)
         bx, by, bz = cp.rotate_3d(data['bx'], data['by'], data['bz'], *los)
         #######################################################################
 
@@ -262,13 +235,11 @@ def calc_all_projections(
             mcf, mcf_bin, mcf_slope, mcf_area, \
             aspect_ratio, cloud_mass, min_den, max_den,\
             binned_den, binned_stoke_Q, binned_stoke_U = \
-            cp.projection(data['den'], x, y, z, bx, by, bz, los)
+            cp.projection(data['density'], bx, by, bz, x, y, z)
 
-        # TODO paramters to store #################
+        # TODO paramters to store #############################################
         # Orientation of the cloud
         all_cloud_orientation += [cloud_orientation]
-        all_aspect_ratio += [aspect_ratio]
-        # Orientation of magnetic field.
         all_b_field_orientation += [b_field_orientation]
         # for mcf
         all_mcf += [mcf]
@@ -276,26 +247,79 @@ def calc_all_projections(
         all_mcf_slope += [mcf_slope]
         all_mcf_area += [mcf_area]
         # min and max column density
+        all_aspect_ratio += [aspect_ratio]
+        all_cloud_masses += [cloud_mass]
         all_min_den += [min_den]
         all_max_den += [max_den]
-        # Cloud and dense threshold
-        all_cloud_masses += [cloud_mass]
         # 2D data map
         all_column_density_map += [binned_den]
         all_stoke_Q_map += [binned_stoke_Q]
         all_stoke_U_map += [binned_stoke_U]
         ###########################################
 
+
+        # TODO save map files due to limitation of memories.  #################
+        # Create output for every 25 projections.
+        if cnt % 25 == 0:
+            
+            out_file = f"r{rank:02d}_{int(cnt/25):02d}"
+            
+            # save for density #################################################
+            print(f"rank {rank: 2d}, saving output file {out_file}_map.h5")
+
+            with h5py.File(os.path.join(out_path, out_file + '_map.h5'), 'w-') as write_data:
+                write_data.create_dataset('los_x', data=losX[cnt-25:cnt])
+                write_data.create_dataset('los_y', data=losY[cnt-25:cnt])
+                write_data.create_dataset('los_z', data=losZ[cnt-25:cnt])
+                write_data.create_dataset('den', data=all_column_density_map)
+                write_data.create_dataset('stoke_Q', data=all_stoke_Q_map)
+                write_data.create_dataset('stoke_U', data=all_stoke_U_map)
+
+            # free memory.
+            del all_column_density_map
+            all_column_density_map = []
+            del all_stoke_Q_map
+            all_stoke_Q_map = []
+            del all_stoke_U_map
+            all_stoke_U_map = []
+            ###################################################################
+            
+            # save for mcf. ###################################################
+            print(f"rank {rank:02d}, saving output file {out_file}_mcf.h5")
+
+            with h5py.File(os.path.join(out_path, out_file + '_mcf.h5'), 'w-') as write_data:
+                write_data.create_dataset('los_x', data=losX[cnt-25:cnt])
+                write_data.create_dataset('los_y', data=losY[cnt-25:cnt])
+                write_data.create_dataset('los_z', data=losZ[cnt-25:cnt])
+                write_data.create_dataset('mcf', data=all_mcf)
+                write_data.create_dataset('mcf_bin', data=all_mcf_bin)
+                
+            # free memory.
+            del all_mcf
+            all_mcf = []
+            del all_mcf_bin
+            all_mcf_bin = []
+        #######################################################################
+
+        # counter
+        cnt += 1
+
     return np.array(all_cloud_orientation), np.array(all_b_field_orientation), \
-        np.array(all_mcf), np.array(all_mcf_bin), np.array(all_mcf_slope), np.array(all_mcf_area), \
+        np.array(all_mcf_slope), np.array(all_mcf_area), \
         np.array(all_aspect_ratio), np.array(all_cloud_masses), \
-        np.array(all_min_den), np.array(all_max_den), \
-        np.array(binned_den), np.array(
-            all_stoke_Q_map), np.array(all_stoke_U_map)
+        np.array(all_min_den), np.array(all_max_den)
+
 
 
 if __name__ == "__main__":
 
-    # 2022-04-11
+    # 2022-04-11 ######################################################
     main('../h5_max_contour/g1040_0016_binary_search.h5', 100, 'fib')
     main('../h5_max_contour/g1041_9015_by_mass.h5', 100, 'fib')
+
+    # main('../h5_max_contour/g1040_0016_binary_search.h5', 10000, 'fib')
+    # main('../h5_max_contour/g1041_9015_by_mass.h5', 10000, 'fib')
+
+    # main('../h5_max_contour/g1040_0016_binary_search.h5', 10000, 'ran')
+    # main('../h5_max_contour/g1041_9015_by_mass.h5', 10000, 'ran')
+    ###################################################################
--- functions/calc_stat_tests.py
+++ functions/calc_stat_tests.py
@@ -26,8 +26,6 @@ def projection_test(n, para, perp):
     prob_pec_paj = np.sum(perp['b_offset'] < 45) / perp['b_offset'].size
     prob_pec_pej = np.sum(perp['b_offset'] > 45) / perp['b_offset'].size    
 
-    print(prob_pac_paj, prob_pac_pej)
-
     # number of parallel cloud
     for k in range(n+1): 
         # number of parallel projection
@@ -52,3 +50,114 @@ def projection_test(n, para, perp):
         std_n_pa += [np.sqrt(np.sum(prob[k]*num_pa_pj[k]**2) - mean_n_pa[-1]**2)]
 
     return prob, num_pa_c, num_pa_pj, np.array(mean_n_pa), np.array(std_n_pa)
+
+
+# Statistical test for mcf slope
+def mcf_slope_test(n, r, para, perp, sampling):
+    
+    np.random.seed(0)
+    
+    mcf_pa = para['mcf_slope']
+    mcf_pe = perp['mcf_slope']
+    
+    bof_pa = para['b_offset']
+    bof_pe = perp['b_offset']
+
+    mcf_pe_pa = mcf_pa[bof_pa > 45]
+    bof_pe_pa = bof_pa[bof_pa > 45]
+    
+    
+    result = []
+    
+    for k in range(n+1):
+        
+        o = max(k-r, 0) # number of parallel cloud gives perpendicular projection
+
+        sample_size = 0
+        
+        curr = []
+        
+        mcf_mean = (k/n)*np.mean(mcf_pa) + ((n-k)/n)*np.mean(mcf_pe)
+        
+        while sample_size < sampling:
+            
+            if o > 0 and mcf_pe_pa.size == 0:
+                break
+            idx_pe_pa = np.random.choice(mcf_pe_pa.size, o, replace = True)
+            idx_pa = np.random.choice(mcf_pa.size, k-o, replace = True)
+            idx_pe = np.random.choice(mcf_pe.size, n-k, replace = True)
+            
+            # ensure number of parallel projection is 5
+            n_para = np.sum(bof_pa[idx_pa] < 45) + np.sum(bof_pe[idx_pe] < 45)
+            if (n_para!= r):
+                continue
+            
+            mcf = np.concatenate([mcf_pe_pa[idx_pe_pa], mcf_pa[idx_pa], mcf_pe[idx_pe]])
+            bof = np.concatenate([bof_pe_pa[idx_pe_pa], bof_pa[idx_pa], bof_pe[idx_pe]])
+            
+            mcf_pa_mean = np.mean(mcf[bof < 45])/mcf_mean
+            mcf_pe_mean = np.mean(mcf[bof > 45])/mcf_mean
+            
+            curr += [mcf_pe_mean - mcf_pa_mean]
+            sample_size += 1
+
+        result += [np.array(curr)]
+        
+    return np.array(result)    
+
+
+# Statistical test for dense gas fraction
+def dense_gass_fraction_test(n, r, para, perp, sampling, dgf_para, dgf_perp):
+    
+    np.random.seed(0)
+    
+    mcf_pa = dgf_para
+    mcf_pe = dgf_perp
+    
+    bof_pa = para['b_offset']
+    bof_pe = perp['b_offset']
+
+    
+    bof_pe_pa = bof_pa[bof_pa > 45]
+    mcf_pe_pa = np.array([mcf_pa]*bof_pe_pa.size) # [bof_pa > 45]
+    
+    
+    result = []
+    
+    for k in range(n+1):
+        
+        o = max(k-r, 0) # number of parallel cloud gives perpendicular projection
+
+        sample_size = 0
+        
+        curr = []
+        
+        mcf_mean = (k/n)*np.mean(mcf_pa) + ((n-k)/n)*np.mean(mcf_pe)
+        
+        while sample_size < sampling:
+            
+            if o > 0 and mcf_pe_pa.size == 0:
+                break
+            idx_pe_pa = np.random.choice(mcf_pe_pa.size, o, replace = True)
+            idx_pa = np.random.choice(mcf_pa.size, k-o, replace = True)
+            idx_pe = np.random.choice(mcf_pe.size, n-k, replace = True)
+            
+            # ensure number of parallel projection is 5
+            n_para = np.sum(bof_pa[idx_pa] < 45) + np.sum(bof_pe[idx_pe] < 45)
+            if (n_para!= r):
+                continue
+            
+            mcf = np.concatenate([mcf_pe_pa[idx_pe_pa], mcf_pa[idx_pa], mcf_pe[idx_pe]])
+            bof = np.concatenate([bof_pe_pa[idx_pe_pa], bof_pa[idx_pa], bof_pe[idx_pe]])
+            
+            mcf_pa_mean = np.mean(mcf[bof < 45])/mcf_mean
+            mcf_pe_mean = np.mean(mcf[bof > 45])/mcf_mean
+            
+            curr += [mcf_pe_mean - mcf_pa_mean]
+            sample_size += 1
+
+        result += [np.array(curr)]
+        
+    return np.array(result)    
+
+
